f9649e8758376bde466052c7de78e796ef14358a
kesaven8 <kesavenvulliamay@gmail.com>
2022-06-20 07:46:40 +0400

    Initial commit

---
+++ persistance/src/main/java/org/j4el/com/entity/Task.java
@@ -0,0 +1,11 @@
+package org.j4el.com.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Task {
+
+    @Id
+    private Long id;
+}
---
+++ rest/src/main/java/org/j4el/com/config/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package org.j4el.com.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.j4el.com")
+public class ApplicationConfig {
+}
